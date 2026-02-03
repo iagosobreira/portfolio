face556e6aecdd3443bf8f3dd0cde0445a8633d2
iagosobreira <githubiago@gmail.com>
2026-02-03 08:05:52 +0100

    Agrego Tailwind config y arregla hovers

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{astro,js,ts,jsx,tsx}',
+    './src/components/**/*.{astro,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
